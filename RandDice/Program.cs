bf0aba6654196e1289beadb259c718c161947af1
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-26 22:29:26 +0000

    Added RandDice Program and Code

---
+++ RandDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random random = new Random(int.Parse(args[1]));
+
+            int n = 0;
+            int M = 0;
+
+            while (n < int.Parse(args[0]))
+            {
+                M += random.Next(1, 7);
+                n++;
+            }
+
+            Console.WriteLine(M);
+        }
+    }
+}
