6eb983c475ae359332b02cb9035b54d421032884
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-26 22:01:57 +0000

    Created Stopwatches

---
+++ Chronos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+        }
+    }
+}
