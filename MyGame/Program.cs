8cc2095b62b9852c990dd2067572b72b5c55739b
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2025-03-27 16:04:47 +0000

    Added Enemy Array

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,9 @@ namespace MyGame
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int numberOfEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[numberOfEnemies];
+            Console.WriteLine($"{numberOfEnemies}");
         }
     }
 }
